02189e6e10a8f35f7267b956a0862bc530cd42a5
Sai Lakshmi Prasanna D <lakshmiprasannadulam041@gmail.com>
2026-08-28 21:16:30 +0530

    Time: 1285 ms (68.83%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,3 +1,4 @@
 # Write your MySQL query statement below
 select p.product_name , s.year, s.price from
-sales s inner join product p on s.product_id=p.product_id;
+sales s inner join product p on s.product_id=p.product_id
+order by p.product_name;
